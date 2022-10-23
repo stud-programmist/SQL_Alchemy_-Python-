78fafa553443116fc2ec45747eedf3081fb2e02b
stud-programmist <65908592+stud-programmist@users.noreply.github.com>
2022-10-23 20:14:22 +0500

    Update config.py

--- SQL_Alchemy_Python/config.py
+++ SQL_Alchemy_Python/config.py
@@ -1,3 +1,2 @@
-DATABASE_URI = 'postgresql+psycopg2://Shupta_EA:20studShEA22@localhost:5432/liv_cooper'
+DATABASE_URI = 'postgresql+psycopg2://Your_login:Your_password:Your_host/liv_cooper'
 
-#DATABASE_URI = 'postgresql+psycopg2://Shupta_EA:20studShEA22@localhost:5432/nichego_org'
